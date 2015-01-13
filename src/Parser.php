d8cd92b8848a84397966c60e3a46cb96453cdb3a
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2015-01-14 00:45:01 +0100

    Flexible configuration for separators
    YAML Front Matter separators (start/end) can be set as strings or as
    array

--- src/Parser.php
+++ src/Parser.php
@@ -30,10 +30,26 @@ class Parser
      */
     private $markdownParser;
 
-    public function __construct(YAMLParser $yamlParser = null, MarkdownParser $markdownParser = null)
-    {
+    /**
+     * @var array
+     */
+    private $startSep;
+
+    /**
+     * @var array
+     */
+    private $endSep;
+
+    public function __construct(
+        YAMLParser $yamlParser = null,
+        MarkdownParser $markdownParser = null,
+        $startSep = '---',
+        $endSep = '---'
+    ) {
         $this->yamlParser = $yamlParser ?: new SymfonyYAMLParser();
         $this->markdownParser = $markdownParser ?: new ParsedownParser();
+        $this->startSep = array_filter((array) $startSep, 'is_string') ?: array('---');
+        $this->endSep = array_filter((array) $endSep, 'is_string') ?: array('---');
     }
 
     /**
@@ -41,36 +57,23 @@ class Parser
      *
      * @param string $str
      * @param bool   $parseMarkdown Should the Markdown be turned into HTML?
-     * @param string $startSep      Front matter separator start
-     * @param string $endSep        Front matter separator end
      *
      * @return Document
      */
-    public function parse($str, $parseMarkdown = true, $startSep = '---', $endSep = '---')
+    public function parse($str, $parseMarkdown = true)
     {
-        $left = '~^'.preg_quote($startSep).'{1}[\r\n|\n]+';
-        $right = preg_quote($endSep).'{1}~s';
+        $yaml = null;
+        $regex = '~^('
+            .implode('|', array_map('preg_quote', $this->startSep)) # $matches[1] start separator
+            .'){1}[\r\n|\n]+(.*)[\r\n|\n]+('                        # $matches[2] between separators
+            .implode('|', array_map('preg_quote', $this->endSep))   # $matches[3] end separator
+            .'){1}[\r\n|\n]*(.*)$~s';                               # $matches[4] document content
 
-        $hasFrontMatter = preg_match($left.'(.*)[\r\n|\n]+'.$right, $str, $matches);
-
-        $yamlContent = $hasFrontMatter ? trim($matches[1]) : false;
-
-        if ($yamlContent === false) {
-            $str = preg_replace($left.$right, '', $str, 1);
-            if ($parseMarkdown) {
-                $str = $this->markdownParser->parse($str);
-            }
-            return new Document(null, $str);
+        if (preg_match($regex, $str, $matches) === 1) { // There is a Front matter
+            $yaml = trim($matches[2]) !== '' ? $this->yamlParser->parse(trim($matches[2])) : null;
+            $str = ltrim($matches[4]);
         }
 
-        // There is a Front matter
-        $yaml = $this->yamlParser->parse($yamlContent);
-        $content = ltrim(preg_replace($left.'(.*)[\r\n|\n]+'.$right, '', $str, 1));
-
-        if ($parseMarkdown) {
-            $content = $this->markdownParser->parse($content);
-        }
-
-        return new Document($yaml, $content);
+        return new Document($yaml, $parseMarkdown ? $this->markdownParser->parse($str) : $str);
     }
 }
