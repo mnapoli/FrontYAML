1cbaa78e4a925e9f7610dcc1640e8b9d6dd2d2e6
Martin Hasoň <martin.hason@gmail.com>
2015-11-16 11:46:09 +0100

    Added support for a new Converter base class for CommonMark

--- src/Bridge/CommonMark/CommonMarkParser.php
+++ src/Bridge/CommonMark/CommonMarkParser.php
@@ -9,6 +9,7 @@
 namespace Mni\FrontYAML\Bridge\CommonMark;
 
 use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Converter;
 use Mni\FrontYAML\Markdown\MarkdownParser;
 
 /**
@@ -18,9 +19,22 @@ use Mni\FrontYAML\Markdown\MarkdownParser;
  */
 class CommonMarkParser implements MarkdownParser
 {
-    public function __construct(CommonMarkConverter $commonMarkConverter = null)
+    public function __construct($commonMarkConverter = null)
     {
-        $this->parser = $commonMarkConverter ?: new CommonMarkConverter();
+        if (null === $commonMarkConverter) {
+            $commonMarkConverter = new CommonMarkConverter();
+        }
+
+        if ((class_exists('League\CommonMark\Converter') && $commonMarkConverter instanceof Converter) || $commonMarkConverter instanceof CommonMarkConverter) {
+            $this->parser = $commonMarkConverter;
+        } else {
+            throw new \InvalidArgumentException(sprintf(
+                'CommonMarkParser::__construct can only accept some converter (%s, %s), "%s" given',
+                'League\CommonMark\Converter',
+                'League\CommonMark\CommonMarkConverter',
+                is_object($commonMarkConverter) ? get_class($commonMarkConverter) : gettype($commonMarkConverter)
+            ));
+        }
     }
 
     /**
